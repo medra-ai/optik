5b9f9a762bcf1a0480df9833af9af9fa90084edc
Kyle Cesare <kcesare@gmail.com>
2023-12-10 17:59:39 -0700

    WIP: Use Divan for benchmarking

--- crates/optik/benches/bench.rs
+++ crates/optik/benches/bench.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use divan::{black_box, Divan};
 use nalgebra::Isometry3;
 
 use optik::*;
@@ -9,80 +9,88 @@ fn load_benchmark_model() -> Robot {
     Robot::from_urdf_str(BENCH_MODEL_STR, "ur_base_link", "ur_ee_link")
 }
 
-fn bench_jacobian(c: &mut Criterion) {
+#[divan::bench]
+fn jacobian(bencher: divan::Bencher) {
     let robot = load_benchmark_model();
     let q = robot.random_configuration(&mut rand::thread_rng());
 
-    c.bench_function("jacobian", |b| {
-        b.iter(|| robot.jacobian_local(black_box(&q)))
+    bencher.bench(|| {
+        robot.jacobian_local(black_box(&q));
     });
 }
 
-fn bench_gradient(c: &mut Criterion) {
-    let robot = load_benchmark_model();
+mod gradient {
+    use super::*;
 
-    let q = robot.random_configuration(&mut rand::thread_rng());
-    let mut g = vec![0.0; q.len()];
-    let tfm_target = Isometry3::identity();
-    let args = ObjectiveArgs {
-        robot,
-        config: SolverConfig::default(),
-        tfm_target,
-    };
+    #[divan::bench]
+    fn analytical(bencher: divan::Bencher) {
+        let robot = load_benchmark_model();
 
-    c.bench_function("gradient_analytical", |b| {
+        let q = robot.random_configuration(&mut rand::thread_rng());
+        let tfm_target = Isometry3::identity();
         let args = ObjectiveArgs {
+            robot: robot.clone(),
             config: SolverConfig {
                 gradient_mode: GradientMode::Analytical,
-                ..args.config
+                ..Default::default()
             },
-            ..args.clone()
+            tfm_target,
         };
-        b.iter(|| objective_grad(black_box(&q), &mut g, &args))
-    });
 
-    c.bench_function("gradient_numerical", |b| {
+        bencher.with_inputs(|| vec![0.0; q.len()]).bench_refs(|g| {
+            objective_grad(black_box(&q), g, &args);
+        });
+    }
+
+    #[divan::bench]
+    fn numerical(bencher: divan::Bencher) {
+        let robot = load_benchmark_model();
+
+        let q = robot.random_configuration(&mut rand::thread_rng());
+        let tfm_target = Isometry3::identity();
         let args = ObjectiveArgs {
+            robot: robot.clone(),
             config: SolverConfig {
                 gradient_mode: GradientMode::Numerical,
-                ..args.config
+                ..Default::default()
             },
-            ..args.clone()
+            tfm_target,
         };
-        b.iter(|| objective_grad(black_box(&q), &mut g, &args))
-    });
+
+        bencher.with_inputs(|| vec![0.0; q.len()]).bench_refs(|g| {
+            objective_grad(black_box(&q), g, &args);
+        });
+    }
 }
 
-fn bench_objective(c: &mut Criterion) {
+#[divan::bench]
+fn objective_fn(bencher: divan::Bencher) {
     let robot = load_benchmark_model();
 
     let q = robot.random_configuration(&mut rand::thread_rng());
     let tfm_target = Isometry3::identity();
     let args = ObjectiveArgs {
-        robot,
+        robot: robot.clone(),
         config: SolverConfig::default(),
         tfm_target,
     };
-    c.bench_function("objective", |b| b.iter(|| objective(black_box(&q), &args)));
+
+    bencher.bench(|| objective(black_box(&q), &args));
 }
 
-fn bench_ik(c: &mut Criterion) {
+#[divan::bench]
+fn solve_ik(bencher: divan::Bencher) {
     let robot = load_benchmark_model();
     let config = SolverConfig::default();
 
     let x0 = vec![0.1, 0.2, 0.0, 0.3, -0.2, -1.1];
     let tfm_target = robot.fk(&[-0.1, -0.2, 0.0, -0.3, 0.2, 1.1]);
 
-    c.bench_function("ik", |b| {
-        b.iter(|| robot.ik(&config, black_box(&tfm_target), x0.clone()))
-    });
+    bencher.bench(|| robot.ik(&config, black_box(&tfm_target), x0.clone()));
 }
 
-criterion_group!(
-    benches,
-    bench_jacobian,
-    bench_gradient,
-    bench_objective,
-    bench_ik
-);
-criterion_main!(benches);
+fn main() {
+    optik::set_parallelism(1);
+
+    Divan::from_args().sample_count(1000).main();
+}
