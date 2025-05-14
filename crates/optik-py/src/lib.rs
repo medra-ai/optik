20afb57901bec01e22723e5c8cefc9965c5eca6d
Paolo Botta <paolobotta@medra.ai>
2025-05-14 10:52:03 -0700

    remove a duplicated method

--- crates/optik-py/src/lib.rs
+++ crates/optik-py/src/lib.rs
@@ -42,19 +42,6 @@ fn pose_to_isometry(v: Option<Vec<f64>>) -> Isometry3<f64> {
     }
 }
 
-fn pose_to_isometry(v: Option<Vec<f64>>) -> Isometry3<f64> {
-    // v is already [px, py, pz, qw, qx, qy, qz]
-
-    if let Some(v) = v {
-        Isometry3::from_parts(
-            Translation3::new(v[0], v[1], v[2]),
-            UnitQuaternion::from_quaternion(Quaternion::new(v[3], v[4], v[5], v[6])),
-        )
-    } else {
-        Isometry3::identity()
-    }
-}
-
 #[pyclass]
 #[pyo3(name = "SolverConfig")]
 struct PySolverConfig(SolverConfig);
